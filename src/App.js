69ae57d9b040b6cc1158ecc54851b2d560144f33
unknown <filipebarbosa94@hotmail.com>
2020-11-05 17:10:11 -0300

    create appp cronometro

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      button: "Vai",
+    };
+
+    this.timer = null;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  vai() {
+    let state = this.state;
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.button = "Vai";
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      }, 100);
+      state.button = "Pausar";
+    }
+    this.setState(state);
+  }
+
+  limpar() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.numero = 0;
+    state.button = "Vai"
+    this.setState(state);
+  }
+  render() {
+    return (
+      <div className="container">
+        <img src={require("./assets/cronometro.png")} className="img" />
+        <a className="timer"> {this.state.numero.toFixed(1)} </a>
+        <div className="area-btn">
+          <a className="button " onClick={this.vai}>
+            {this.state.button}
+          </a>
+          <a className="button" onClick={this.limpar}>
+            limpar
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
