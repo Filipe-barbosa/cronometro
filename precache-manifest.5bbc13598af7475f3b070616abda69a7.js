98ef695e88112370cf014a06ab1aeeeb51fc6d0d
unknown <filipebarbosa94@hotmail.com>
2020-11-05 18:39:55 -0300

    Updates

---
+++ precache-manifest.5bbc13598af7475f3b070616abda69a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2534647ac5515347e9eba7415d58f584",
+    "url": "/cronometro/index.html"
+  },
+  {
+    "revision": "94f891539377738716c0",
+    "url": "/cronometro/static/css/main.191d8f49.chunk.css"
+  },
+  {
+    "revision": "f915b40fbf83b21f2794",
+    "url": "/cronometro/static/js/2.2f0b1237.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cronometro/static/js/2.2f0b1237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f891539377738716c0",
+    "url": "/cronometro/static/js/main.0752fdd9.chunk.js"
+  },
+  {
+    "revision": "7cd40cdbcbc650b7f0a8",
+    "url": "/cronometro/static/js/runtime-main.15da910d.js"
+  },
+  {
+    "revision": "54825791bba9807d5e4975c702d4d4d0",
+    "url": "/cronometro/static/media/cronometro.54825791.png"
+  }
+]);
